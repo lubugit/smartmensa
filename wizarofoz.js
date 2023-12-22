e62f90a05911f415bc0ad029b2eaac9a35fb7eb8
LuBu <lubu.github@gmail.com>
2023-12-22 20:16:49 +0100

    WOZ
    - WOZ Seite erstellt
    - Funktion für Sprachausgabe erstellt
    - Scann mich dummy erstellt
    - Splitscreen dummy erstellt

---
+++ wizarofoz.js
@@ -0,0 +1,62 @@
+const ttsAreaParts = [
+    ["Gerichte im Startscreen", "Heute am Mittwoch gibt es Pizza, Currywurst und Pommes, Bratfisch und Hamburger"],
+    ["Möglichkeiten im Startscreen", "Möchtest du dich scannen lassen?"],
+    ["Empfehlungen", "Ich empfehle dir Pizza oder Hamburger"],
+    ["Sprache wechseln", "Möchtest du auf Deutsch oder Englisch arbeiten?"],
+    ["Pizza vorlesen", "Ich kann dir mehr zur Pizza erzählen ..."],
+    ["Timer gestartet", "Es wurden mehrere Personen erkannt, du hast noch 20 Sekunden vor dem Bildschirm."],
+    ["Timer abgelaufen", "Dein Timer ist abgelaufen, bitte entferne dich vom Bildschirm."]
+]
+
+// after loading the website completly
+window.onload = function(){
+    generateTTSArea();
+}
+
+/**
+ * Simulation of succesfully scanning of a person.
+ * 
+ * @param {*} select HTML select object
+ */
+function scanPerson(select){
+    const value = select.value;
+    const person = select[value].text;
+
+    console.log("You selected " + person + "("+value+")");
+}
+
+/**
+ * Simulation of recognition of multiple people.
+ */
+function toggleSplitscreen(box){
+    console.log("Toggled splitscreen");
+
+    if(box.checked){
+        console.log("Splitscreen on");
+    }else{
+        console.log("Splitscreen off");
+    }
+}
+
+/**
+ * To read content aloud in the language.
+ * @param {*} content Content to read aloud
+ * @param {*} language Language to read aloud
+ */
+function readAloud(content, lang){
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = content;
+    msg.lang = "de";
+    window.speechSynthesis.speak(msg);
+}
+
+/**
+ * Filling the text-to-speach area with usable buttons.
+ */
+function generateTTSArea(){
+    const ttsArea = document.getElementById("ttsArea");
+
+    for(i = 0; i < ttsAreaParts.length;i++){
+        ttsArea.innerHTML += "<button onclick=\"readAloud('"+ ttsAreaParts[i][1]+ "')\">"+ ttsAreaParts[i][0]+ "</button>";
+    }
+}
