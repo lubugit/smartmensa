0976b4abd501e6a854f4510edd00de161ce8ff69
LuBu <lubu.github@gmail.com>
2024-01-05 22:55:42 +0100

    Include WOZ
    - included splitscreen
    - food filter

--- smartmensa.js
+++ smartmensa.js
@@ -42,13 +42,15 @@ var intervalID = setInterval(update, 1000);
 var dbname = "smartmensa";
 var dburl = "http://127.0.0.1:5984/" + dbname + "/";
 
+var profile = {};
+
 var handlers = {
     "profile": updateProfile,
-    "speechOutput": readAloud
+    "speechOutput": readAloud,
+    "splitscreen": updateSplitscreen
 }
 function updateProfile(response){
-    console.log(response);
-    //document.getElementById("test").innerHTML = response.name;
+    profile = response;
 }
 function readAloud(response){
     if(response.text != ""){
@@ -58,6 +60,20 @@ function readAloud(response){
         window.speechSynthesis.speak(msg);
     }
 }
+
+function updateSplitscreen(response){
+    const splitscreen = document.getElementById("splitscreen");
+    const normalscreen = document.getElementById("normalscreen");
+
+    if(response.enabled){
+        normalscreen.style.width = "50%";
+        splitscreen.style.display = "block";
+    }else{
+        normalscreen.style.width = "100%";
+        splitscreen.style.display = "none";
+    }
+}
+
 /**
  * Seconds timer.
  * @param {*} sec how many seconds you have
@@ -79,4 +95,31 @@ function timer(sec){
 
 function scan(){
     get('profile');
+}
+
+function loadFoodContent(){
+    fetch("./gerichte_real.json")
+        .then((response) => response.json())
+        .then((json) => {
+            const foodcontainer = document.getElementById("foodcontainer");
+            const falseFoodContainer = document.getElementById("falseFood");
+
+            var sortedJson = [];
+
+            for(var i = 0; i < json.length;i++){
+                if(profile.favourites.includes(json[i].id)){
+                    sortedJson.unshift(json[i]);
+                }else{
+                    sortedJson.push(json[i]);
+                }
+            }
+
+            for(var i = 0; i < sortedJson.length;i++){
+                if(!profile.intolerances.some(val => sortedJson[i].allergens.includes(val))){
+                    foodcontainer.innerHTML += '<div class="card food-card" onclick="getinfos(this)" id="' + sortedJson[i].id +'"><img src="' + sortedJson[i].picture +'" alt="Food ' + i +'"><div class="card-description">'+ sortedJson[i].translatedName + '</div></div>'
+                }else{
+                    falseFoodContainer.innerHTML += '<div class="card food-card" onclick="getinfos(this)" id="' + sortedJson[i].id +'"><img src="' + sortedJson[i].picture +'" alt="Food ' + i +'"><div class="card-description">'+ sortedJson[i].translatedName + '</div></div>'
+                }
+            }
+        });
 }
